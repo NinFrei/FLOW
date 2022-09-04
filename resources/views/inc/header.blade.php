92d357efe6fee70659ff4cf396476087099f51be
NinFrei <n.freiman@yahoo.com>
2022-09-04 16:21:12 +0200

    geändert:       public/css/app.css	geändert:       resources/sass/customstyles.scss 	geändert:       resources/views/inc/header.blade.php

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,5 +1,4 @@
 @section('header')
-<div class="wrapper">
     <div class="header1 container-XXL">
         <div class="row">
         <div class="col-md-4">
@@ -13,26 +12,26 @@
     </div>
 
 
-    <div class="my-0 mr-md-auto font-weight-normal"></a>
+    <div class=" align-bottom my-1 mr-md-auto font-weight-normal bg-white"></a>
         <!--<div class="navbar navbar-inverse navbar-fixed-top">
                 <div class="contaiter">
                     <div class="navbar-header">-->
 
 
 
-        <nav class="navbar navbar-nev navbar-center rexpand-sm justify-content-center my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="{{ route('home') }}">Home</a>
-            <a class="p-2 text-dark" href="{{ route('modetrends') }}">Modetrends</a>
-            <a class="p-2 text-dark" href="{{ route('beauty') }}">Beauty</a>
-            <a class="p-2 text-dark" href="{{ route('empowerment') }}">Female empowerment</a>
-            <a class="p-2 text-dark" href="{{ route('lifestyle') }}">Lifestyle</a>
-            <a class="p-2 text-dark" href="{{ route('shop') }}">Shop</a>
-            <a class="p-2 text-dark" href="{{ route('aboutme') }}">About me</a>
-            <a class="p-2 text-dark" href="{{ route('contact') }}">Contact</a>
-            <a class="p-2 text-dark" href="{{ route('contact-data') }}">Nachricht</a>
+        <nav class="navbar navbar-nev navbar-center rexpand-sm justify-content-center my-2 my-md-0 mr-md-3 navbar1">
+            <a class="p-2 px-5 text-dark" href="{{ route('home') }}">Home</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('modetrends') }}">Modetrends</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('beauty') }}">Beauty</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('empowerment') }}">Female empowerment</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('lifestyle') }}">Lifestyle</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('shop') }}">Shop</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('aboutme') }}">About me</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('contact') }}">Contact</a>
+            <a class="p-2 px-5 text-dark" href="{{ route('contact-data') }}">Nachricht</a>
         </nav>
 
     </div>
     </div>
     </div>
-    </div>
+
